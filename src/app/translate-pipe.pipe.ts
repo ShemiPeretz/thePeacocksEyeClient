22160cce075480a47c4f1cfde9395c292bfe6b6f
ShemiPeretz <shemi.p17@gmail.com>
2024-03-16 13:30:34 +0200

    Css changes

--- src/app/translate-pipe.pipe.ts
+++ src/app/translate-pipe.pipe.ts
@@ -6,10 +6,16 @@ import * as translation from "../assets/he.json";
 })
 export class TranslatePipePipe implements PipeTransform {
   translation:any;
+  translate: boolean;
+
 constructor() {
   this.translation=translation;
+  this.translate = false;
 }
   transform(value: string): string {
+    if (!this.translate){
+      return value;
+    }
     const hebTranslation = this.translation[value.toLowerCase()];
     if (hebTranslation) {
       return hebTranslation;
