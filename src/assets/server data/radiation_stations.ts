51994b56afdc4b000165711893296cf62052a809
ShemiPeretz <shemi.p17@gmail.com>
2024-07-12 11:16:58 +0300

    Added files

---
+++ src/assets/server data/radiation_stations.ts
@@ -0,0 +1,13 @@
+export const RADIATION_STATIONS: { [key: string]: number[] } = {
+    "ARAD": [7822],
+    "BEER SHEVA UNI": [7838, 7839],
+    "BESOR FARM": [3891],
+    "BET DAGAN RAD": [2524, 2525],
+    "ELAT": [9974],
+    "HAIFA TECHNION": [850],
+    "JERUSALEM GIVAT RAM": [7020],
+    "MIZPE RAMON": [8213],
+    "SEDE BOQER UNI": [8209],
+    "SEDOM": [9571],
+    "YOTVATA": [9772],
+};
